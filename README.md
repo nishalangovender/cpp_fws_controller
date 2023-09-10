9bba878d2a034efc0f7479620e7c593dc63eaab6
nishalangovender <nishalan.govender@gmail.com>
2023-09-10 20:05:01 +0200

    Working fws_controller with Steering

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
-# fws_controller
+## fws_controller
 
-ROS2 Humble 4WS Controller for ROS2 Control
+ROS2 Humble 4WS Controller for ROS2 Control.
+
+NOTE: The information following is from Differential Drive Controller (https://control.ros.org/master/doc/ros2_controllers/diff_drive_controller/doc/userdoc.html) and will be updated to this controller.
+
+==========================================================================================================================================================================================================
+
+Controller for mobile robots with 4WS drive.
+
+As input it takes heading and velocity commands for the robot body, which are translated to steering and wheel commands for the 4WS drive base.
+
+Odometry is computed from hardware feedback and published.
+
+## Controller Interfaces
+
+# Feedback
+
+As feedback interface type the joints' position (hardware_interface::HW_IF_POSITION) or velocity (hardware_interface::HW_IF_VELOCITY, if parameter position_feedback=false) are used.
+
+# Output
+
+Joints' position (hardware_interface::HW_IF_POSITION) and velocity (hardware_interface::HW_IF_VELOCITY) are used.
+
+## ROS2 Interfaces
+
+# Subscribers
+
+~/cmd_vel [geomtry_msgs/msg/TwistStamped]
+Velocity command for the controller, if use_stamped_vel=true. The controller extracts the x component of the linear velocity and the z component of the angular velocity. Velocities on other components are ignored.
+
+~/cmd_vel_unstamped [geometry_msgs::msg::Twist]
+Velocity command for the controller, if use_stamped_vel=false. The controller extracts the x component of the linear velocity and the z component of the angular velocity. Velocities on other components are ignored.
+
+# Publishers
+
+~/odom [nav_msgs::msg::Odometry]
+This represents an estimate of the robot’s position and velocity in free space.
+
+/tf [tf2_msgs::msg::TFMessage]
+tf tree. Published only if enable_odom_tf=true
+
+~/cmd_vel_out [geometry_msgs/msg/TwistStamped]
+Velocity command for the controller, where limits were applied. Published only if publish_limited_velocity=true
+
+# Parameters
+
+linear.x [JointLimits structure]
+Joint limits structure for the linear X-axis. The limiter ignores position limits. For details see joint_limits package from ros2_control repository.
+
+angular.z [JointLimits structure]
+Joint limits structure for the rotation about Z-axis. The limiter ignores position limits. For details see joint_limits package from ros2_control repository.
--- src/fws_controller.cpp
+++ src/fws_controller.cpp
@@ -1,25 +1,17 @@
 // Nishalan Govender
 // Autonomous Parking of a 4WS Vehicle
 
-#include "fws_controller/fws_controller.hpp"
-
-#include <stddef.h>
-#include <algorithm>
-#include <memory>
 #include <string>
-#include <vector>
 
-#include "rclcpp/qos.hpp"
-#include "rclcpp/time.hpp"
-#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
-#include "rclcpp_lifecycle/state.hpp"
-
-using config_type = controller_interface::interface_configuration_type;
+#include "fws_controller/fws_controller.hpp"
 
 namespace fws_controller
 {
 
-  FWSController::FWSController() : controller_interface::ControllerInterface() {}
+  FWSController::FWSController() : position_controllers::JointGroupPositionController()
+  {
+    interface_name_ = hardware_interface::HW_IF_POSITION;
+  }
 
   // =============================================================================================================================== //
 
@@ -30,239 +22,23 @@ namespace fws_controller
   controller_interface::CallbackReturn FWSController::on_init()
   {
 
-    // Add Error Handling
-
-    joint_names_ = auto_declare<std::vector<std::string>>("joints", joint_names_);
-    command_interface_types_ =
-        auto_declare<std::vector<std::string>>("command_interfaces", command_interface_types_);
-
-    state_interface_types_ =
-        auto_declare<std::vector<std::string>>("state_interfaces", state_interface_types_);
-
-    point_interp_.positions.assign(joint_names_.size(), 0);
-    point_interp_.velocities.assign(joint_names_.size(), 0);
-
-    return CallbackReturn::SUCCESS;
-  }
-
-  // =============================================================================================================================== //
-
-  // Command Interface Configuration
-
-  // =============================================================================================================================== //
-
-  controller_interface::InterfaceConfiguration FWSController::command_interface_configuration()
-      const
-  {
-
-    controller_interface::InterfaceConfiguration conf = {config_type::INDIVIDUAL, {}};
-
-    conf.names.reserve(joint_names_.size() * command_interface_types_.size());
-    for (const auto &joint_name : joint_names_)
+    auto ret = position_controllers::JointGroupPositionController::on_init();
+    if (ret != CallbackReturn::SUCCESS)
     {
-      for (const auto &interface_type : command_interface_types_)
-      {
-        conf.names.push_back(joint_name + "/" + interface_type);
-      }
+      return ret;
     }
 
-    return conf;
-  }
-
-  // =============================================================================================================================== //
-
-  // State Interface Configuration
-
-  // =============================================================================================================================== //
-
-  controller_interface::InterfaceConfiguration FWSController::state_interface_configuration() const
-  {
-    controller_interface::InterfaceConfiguration conf = {config_type::INDIVIDUAL, {}};
-
-    conf.names.reserve(joint_names_.size() * state_interface_types_.size());
-    for (const auto &joint_name : joint_names_)
+    try
     {
-      for (const auto &interface_type : state_interface_types_)
-      {
-        conf.names.push_back(joint_name + "/" + interface_type);
-      }
+      get_node()->set_parameter(
+          rclcpp::Parameter("interface_name", hardware_interface::HW_IF_POSITION));
+    }
+    catch (const std::exception &e)
+    {
+      fprintf(stderr, "Exception thrown during init stage with message: %s \n", e.what());
+      return CallbackReturn::ERROR;
     }
 
-    return conf;
-  }
-
-  // =============================================================================================================================== //
-
-  // On Configure
-
-  // =============================================================================================================================== //
-
-  controller_interface::CallbackReturn FWSController::on_configure(const rclcpp_lifecycle::State &)
-  {
-    auto callback =
-        [this](const std::shared_ptr<trajectory_msgs::msg::JointTrajectory> traj_msg) -> void
-    {
-      traj_msg_external_point_ptr_.writeFromNonRT(traj_msg);
-      new_msg_ = true;
-    };
-
-    joint_command_subscriber_ =
-        get_node()->create_subscription<trajectory_msgs::msg::JointTrajectory>(
-            "~/joint_trajectory", rclcpp::SystemDefaultsQoS(), callback);
-
-    return CallbackReturn::SUCCESS;
-  }
-
-  // =============================================================================================================================== //
-
-  // On Activate
-
-  // =============================================================================================================================== //
-
-  controller_interface::CallbackReturn FWSController::on_activate(const rclcpp_lifecycle::State &)
-  {
-    // clear out vectors in case of restart
-    joint_position_command_interface_.clear();
-    joint_velocity_command_interface_.clear();
-    joint_position_state_interface_.clear();
-    joint_velocity_state_interface_.clear();
-
-    // assign command interfaces
-    for (auto &interface : command_interfaces_)
-    {
-      command_interface_map_[interface.get_interface_name()]->push_back(interface);
-    }
-
-    // assign state interfaces
-    for (auto &interface : state_interfaces_)
-    {
-      state_interface_map_[interface.get_interface_name()]->push_back(interface);
-    }
-
-    return CallbackReturn::SUCCESS;
-  }
-
-  void interpolate_point(
-      const trajectory_msgs::msg::JointTrajectoryPoint &point_1,
-      const trajectory_msgs::msg::JointTrajectoryPoint &point_2,
-      trajectory_msgs::msg::JointTrajectoryPoint &point_interp, double delta)
-  {
-    for (size_t i = 0; i < point_1.positions.size(); i++)
-    {
-      point_interp.positions[i] = delta * point_2.positions[i] + (1.0 - delta) * point_2.positions[i];
-    }
-    for (size_t i = 0; i < point_1.positions.size(); i++)
-    {
-      point_interp.velocities[i] =
-          delta * point_2.velocities[i] + (1.0 - delta) * point_2.velocities[i];
-    }
-  }
-
-  void interpolate_trajectory_point(
-      const trajectory_msgs::msg::JointTrajectory &traj_msg, const rclcpp::Duration &cur_time,
-      trajectory_msgs::msg::JointTrajectoryPoint &point_interp)
-  {
-    double traj_len = traj_msg.points.size();
-    auto last_time = traj_msg.points[traj_len - 1].time_from_start;
-    double total_time = last_time.sec + last_time.nanosec * 1E-9;
-
-    size_t ind = cur_time.seconds() * (traj_len / total_time);
-    ind = std::min(static_cast<double>(ind), traj_len - 2);
-    double delta = cur_time.seconds() - ind * (total_time / traj_len);
-    interpolate_point(traj_msg.points[ind], traj_msg.points[ind + 1], point_interp, delta);
-  }
-
-  // =============================================================================================================================== //
-
-  // Update
-
-  // =============================================================================================================================== //
-
-  controller_interface::return_type FWSController::update(
-      const rclcpp::Time &time, const rclcpp::Duration & /*period*/)
-  {
-    if (new_msg_)
-    {
-      trajectory_msg_ = *traj_msg_external_point_ptr_.readFromRT();
-      start_time_ = time;
-      new_msg_ = false;
-    }
-
-    if (trajectory_msg_ != nullptr)
-    {
-      interpolate_trajectory_point(*trajectory_msg_, time - start_time_, point_interp_);
-      for (size_t i = 0; i < joint_position_command_interface_.size(); i++)
-      {
-        joint_position_command_interface_[i].get().set_value(point_interp_.positions[i]);
-      }
-      for (size_t i = 0; i < joint_velocity_command_interface_.size(); i++)
-      {
-        joint_velocity_command_interface_[i].get().set_value(point_interp_.velocities[i]);
-      }
-    }
-
-    return controller_interface::return_type::OK;
-  }
-
-  // controller_interface::return_type FWSController::update()
-  // {
-
-  // Control Logic
-  // Side-Slip Angle
-  // double beta = atan(0.5 * (tan(delta_A) + tan(delta_B)));
-
-  // Vehicle Velocity
-  // double V = (v_A * cos(delta_A) + v_B * cos(delta_B)) / (2.0 * cos(beta));
-
-  // Heading Angle
-  // double L =  // Replace with the distance between the front and rear axles of your vehicle
-  // double psi = (V * cos(beta) * (tan(delta_A) + tan(delta_B))) / L;
-
-  // Commands
-  // double front_steering_command = delta_A_;
-  // double rear_steering_command = delta_B_;
-  // double front_wheel_velocity_command = sqrt(v_A_) / 2.0;
-  // double rear_wheel_velocity_command = sqrt(v_B_) / 2.0;
-
-  // Set Joint Commands
-  // front_left_steering_joint_.set_command(front_steering_command);
-  // front_right_steering_joint_.set_command(front_steering_command);
-  // rear_left_steering_joint_.set_command(rear_steering_command);
-  // rear_right_steering_joint_.set_command(rear_steering_command);
-
-  // front_left_wheel_joint_.set_command(front_wheel_velocity_command);
-  // front_right_wheel_joint_.set_command(front_wheel_velocity_command);
-  // rear_left_wheel_joint_.set_command(rear_wheel_velocity_command);
-  // rear_right_wheel_joint_.set_command(rear_wheel_velocity_command);
-
-  // return controller_interface::return_type::OK;
-  // }
-
-  // =============================================================================================================================== //
-
-  // Deactivate Cleanup Error Shutdown
-
-  // =============================================================================================================================== //
-
-  controller_interface::CallbackReturn FWSController::on_deactivate(const rclcpp_lifecycle::State &)
-  {
-    release_interfaces();
-
-    return CallbackReturn::SUCCESS;
-  }
-
-  controller_interface::CallbackReturn FWSController::on_cleanup(const rclcpp_lifecycle::State &)
-  {
-    return CallbackReturn::SUCCESS;
-  }
-
-  controller_interface::CallbackReturn FWSController::on_error(const rclcpp_lifecycle::State &)
-  {
-    return CallbackReturn::SUCCESS;
-  }
-
-  controller_interface::CallbackReturn FWSController::on_shutdown(const rclcpp_lifecycle::State &)
-  {
     return CallbackReturn::SUCCESS;
   }
 
